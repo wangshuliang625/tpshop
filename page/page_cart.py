d4e39923fc6a4cd84fa6efb506fed7640b834339
wangshuliang <wangshuliang0625@163.com>
2021-03-09 17:56:57 +0800

    完善购物车模块业务代码

--- page/page_cart.py
+++ page/page_cart.py
@@ -3,37 +3,44 @@
 # @Author : WSL
 # @File : page_cart.py
 from base.base import Base
+import page
 
 
 class PageCart(Base):
 
     # 打开首页
     def page_open_index(self):
-        pass
+        self.base_index()
 
     # 输入搜索内容
-    def page_input_search(self):
-        pass
+    def page_input_search(self, value='小米'):
+        self.base_input(page.cart_search, value)
 
     # 点击搜索按钮
     def page_click_search_btn(self):
-        pass
+        self.base_click(page.cart_search_btn)
 
     # 点击添加购物车 跳转到商品详情页
     def page_click_add_cart_info(self):
-        pass
+        self.page_click_add_cart_info()
 
     # 点击添加购物车
     def page_click_add_cart(self):
-        pass
+        self.page_click_add_cart()
 
     # 获取添加结果
     def page_get_text(self):
-        pass
+        # 获取frame表单
+        self.base_switch_frame(self.base_find(page.cart_frame_id))
+        # 获取结果并返回
+        self.base_get_text(page.cart_add_result)
 
     # 关闭窗口
     def page_close_window(self):
-        pass
+        # 回到默认目录
+        self.base_default_content()
+        # 点击关闭操作
+        self.base_click(page.cart_close_window)
 
     # 组合业务调用方法
     def page_add_cart(self):
